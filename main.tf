e7437449e1f5b24ac980ce5286552bf3927c488b
twistedgrim <rebel623@gmail.com>
2019-01-09 21:37:41 -0600

    Reaper file/configuration sync update 01/09/2019 21:37:41

--- main.tf
+++ main.tf
@@ -1,3 +1,30 @@
+/**
+ *# aws-terraform-sqs
+ *
+ *This module sets up a sqs-queue with varying options including deadletter, fifo, and ecryption.
+ *
+ *## Basic Usage
+ *
+ *```
+ *module "standard_queue" {
+ *  source                      = "git@github.com:rackspace-infrastructure-automation/aws-terraform-sqs//?ref=v0.0.2"
+ *  name                        = "myqueue"
+ *  delay_seconds               = 90
+ *  max_message_size            = 2048
+ *  message_retention_seconds   = 86400
+ *  receive_wait_time_seconds   = 10
+ *  enable_sqs_queue_policy     = true
+ *  role_arn                    = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/Rackspace"
+ *  create_internal_zone_record = true
+ *  internal_record_name        = "myqueue"
+ *  internal_zone_name          = "testqueues.local"
+ *  route_53_hosted_zone_id     = "${aws_route53_zone.testing-zone.zone_id}"
+ *}
+ *```
+ *
+ * Full working references are available at [examples](examples)
+ */
+
 locals {
   tags {
     Name            = "${var.name}"
